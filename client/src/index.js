91c16c284f484d516af6804892e36b1068e83095
Alexandre-st <alexandre.saintprix@gmail.com>
2022-10-18 12:09:21 +0200

    Add the logic with redux tollkit for the login/register form

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './app/store';
+
+// Local import 
 import App from './App';
 
 const app = document.getElementById('root');
 const root = createRoot(app);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
